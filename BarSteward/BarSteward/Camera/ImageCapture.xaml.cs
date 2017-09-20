5a0cf4ce5cd168abb378a06a5b76411f6a5444ee
Morgan Peat <morgan.peat@rbs.com>
2017-09-20 06:53:16 +0100

    trying to get it working

---
+++ BarSteward/BarSteward/Camera/ImageCapture.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+
+namespace BarSteward.Camera
+{
+    /// <summary>
+    /// Interaction logic for ImageCapture.xaml
+    /// </summary>
+    public partial class ImageCapture : UserControl, INotifyPropertyChanged
+    {
+        private readonly DispatcherTimer _timer;
+        private readonly VideoCapture _capture;
+
+        public ImageCapture()
+        {
+            InitializeComponent();
+
+            _capture = new VideoCapture();
+
+            //framerate of 10fps
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(100) };
+            _timer.Tick += async (object s, EventArgs a) =>
+            {
+                //draw the image obtained from camera
+                using (Mat frame = _capture.QueryFrame())
+                {
+                    if (frame != null)
+                    {
+                        Image<Bgr, byte> image = frame.ToImage<Bgr, byte>();
+                        CurrentFrame = image.ToBitmap();
+                    }
+                }
+            };
+            _timer.Start();
+        }
+
+        private Bitmap _currentFrame;
+        public Bitmap CurrentFrame
+        {
+            get => _currentFrame;
+            private set
+            {
+                if (_currentFrame != value)
+                {
+                    _currentFrame = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
