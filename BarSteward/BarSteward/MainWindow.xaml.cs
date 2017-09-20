ff3b3df9b3d5067bbd292484189a339c2eda9705
Morgan Peat <morgan.peat@rbs.com>
2017-09-20 08:20:16 +0100

    neater

--- BarSteward/BarSteward/MainWindow.xaml.cs
+++ BarSteward/BarSteward/MainWindow.xaml.cs
@@ -1,89 +1,18 @@
-﻿using System;
-using System.ComponentModel;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Runtime.CompilerServices;
-using System.Windows;
-using System.Windows.Media.Imaging;
-using System.Windows.Threading;
-using Emgu.CV;
-using Emgu.CV.Structure;
+﻿using System.Windows;
 
 namespace BarSteward
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        private readonly DispatcherTimer _timer;
-        private readonly VideoCapture _capture;
-
+      
         public MainWindow()
         {
-            InitializeComponent();
-            _capture = new VideoCapture();
-
-            //framerate of 10fps
-            _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(100) };
-            _timer.Tick += async (object s, EventArgs a) =>
-            {
-                //draw the image obtained from camera
-                using (Mat frame = _capture.QueryFrame())
-                {
-                    if (frame != null)
-                    {
-                        using (Image<Bgr, byte> image = frame.ToImage<Bgr, byte>())
-                        {
-                            CurrentFrame = Convert( image.ToBitmap());
-                            //imageCtrl.Source = Convert(CurrentFrame);
-                        }
-                    }
-                }
-            };
-            _timer.Start();
-
+            InitializeComponent();      
             this.DataContext = this;
         }
-        private BitmapSource _currentFrame;
-        public BitmapSource CurrentFrame
-        {
-            get => _currentFrame;
-            private set
-            {
-                if (_currentFrame != value)
-                {
-                    //_currentFrame?.Dispose();
-                    _currentFrame = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private BitmapImage Convert(Bitmap bitmap)
-        {
-            using (MemoryStream memory = new MemoryStream())
-            {
-                bitmap.Save(memory, ImageFormat.Bmp);
-                memory.Position = 0;
-
-                BitmapImage bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
-
-                return bitmapImage;
-            }
-        }
+        
     }
 }
